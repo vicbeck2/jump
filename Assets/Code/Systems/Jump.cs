1df3c2a9348d8b642d7494bc100a98bfc2d4f943
vicbeck2 <vic.beck@gmx.com>
2023-08-03 01:00:18 +0200

    restore

--- Assets/Code/Systems/Jump.cs
+++ Assets/Code/Systems/Jump.cs
@@ -1,20 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 public class Jump : MonoBehaviour
 {
 	public GameObject floor;
 	public Vector3 force;
 	bool can_jump;
-
-	[DllImport("__Internal")]
-	private static extern void Hello();
+	public Text log;
+	public Text chain;
 
 	void Start()
 	{
-		Hello();
+		Web3.Initialize();
+		Web3.Connect();
+		Web3.SwitchNetwork("0x5");
+		Web3.OnAccountChange += Log;
+		Web3.OnChainChange += ChainChange;
+	}
+
+	public void Log (string text)
+	{
+		log.text = text;
+	}
+
+	public void ChainChange (string text)
+	{
+		chain.text = text;
 	}
 
 	void Update()
